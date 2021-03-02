2b6258d737bf0b94ecef083f7746db80ecad78d6
Kathryn Wilkinson <kathrynxwilk@gmail.com>
2021-03-02 13:58:43 -0600

    Docs: add script pseudocode

---
+++ assets/script.js
@@ -0,0 +1,19 @@
+// declare variables
+
+//render search history from local storage on page load
+
+//event listener on search button to fetch data based on search input value 
+
+
+//functions to render fetched data on page//
+    //top current forecast
+
+    //bottom 5-day forecast
+
+
+//function to render search history
+    //append child
+
+    //link to current weather conditions and render in forecast cards
+
+//store search history in local storage
