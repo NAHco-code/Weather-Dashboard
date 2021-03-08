919d4e5443324cfd2708efe1d4ab3a915284ac42
Kathryn Wilkinson <kathrynxwilk@gmail.com>
2021-03-08 17:47:42 -0600

    Function Build: append data to DOM to populate forecast div; use moment to populate dates

--- assets/script.js
+++ assets/script.js
@@ -23,6 +23,21 @@ const weekForecast = [{
 
 //To Do - render search history from local storage on page load
 
+//dates
+let today = document.querySelector('#today');
+today.textContent = moment().format('ll');
+let tomorrow = document.querySelector('#date_1');
+tomorrow.textContent = moment().add(1, 'days').format('ll');
+let tomorrow2 = document.querySelector('#date_2');
+tomorrow2.textContent = moment().add(2, 'days').format('ll');
+let tomorrow3 = document.querySelector('#date_3');
+tomorrow3.textContent = moment().add(3, 'days').format('ll');
+let tomorrow4 = document.querySelector('#date_4');
+tomorrow4.textContent = moment().add(4, 'days').format('ll');
+let tomorrow5 = document.querySelector('#date_5');
+tomorrow5.textContent = moment().add(5, 'days').format('ll');
+
+
 //define function to access current weather conditions
 function getCurrent(cityInput) {
 
@@ -38,14 +53,12 @@ function getCurrent(cityInput) {
                 let cityName = document.querySelector('#city_name');
                 cityName.textContent = data.name;
 
-                // let today = document.querySelector('#today');
-                // today.innerHTML = '00.00.0000'; NOT WORKING
-
                 // let icon = document.querySelector('#current_icon');
-                // icon.textContent = data.weather.icon; NOT WORKING
+                // let iconCall = data.weather[0].icon;
+                // icon.innerHTML = '<img src=`http://openweathermap.org/img/wn/`' +iconCall+ '.png id="current_icon" alt="weather icon"/>';
 
                 let temp = document.querySelector('#current_temp');
-                temp.textContent = 'Temperature: ' + data.main.temp + ' F';
+                temp.textContent = 'Temperature: ' + data.main.temp + ' f';
 
                 let humidity = document.querySelector('#current_humid');
                 humidity.textContent = 'Humidity:  ' + data.main.humidity + '%';
@@ -57,7 +70,6 @@ function getCurrent(cityInput) {
 
                 //testing
                 console.log(data.weather[0].icon);
-                // console.log(data.main.humidity);
 
                 //call function to render 5 day forecast in this function for access to latitude and longitude from city search in current weather function
                 getForecast(data.coord.lat, data.coord.lon);
@@ -82,11 +94,38 @@ function getForecast(latitude, longitude) {
                 let uvIndex = document.querySelector('#current_uv');
                 uvIndex.textContent = 'UV Index: ' + data.current.uvi;
 
-                // let today = document.querySelector('#today');
-                // today.innerHTML = '00.00.0000';
-
                 // let icon = document.querySelector('#current_icon');
                 // icon.textContent = data.weather.icon;
+
+                //temp forecast
+                //TODO for loop
+                // for (i = 0; i < forecast.length; i++)
+
+                let temp1 = document.querySelector('#temp_1');
+                temp1.textContent = 'Temp: ' + data.daily[1].temp.day + ' f';
+                let temp2 = document.querySelector('#temp_2');
+                temp2.textContent = 'Temp: ' + data.daily[2].temp.day + ' f';
+                let temp3 = document.querySelector('#temp_3');
+                temp3.textContent = 'Temp: ' + data.daily[3].temp.day + ' f';
+                let temp4 = document.querySelector('#temp_4');
+                temp4.textContent = 'Temp: ' + data.daily[4].temp.day + ' f';
+                let temp5 = document.querySelector('#temp_5');
+                temp5.textContent = 'Temp: ' + data.daily[5].temp.day + ' f';
+
+
+                //humidity forecast
+                //TODO for loop
+                // for (i = 0; i < forecast.length; i++)
+                let humid1 = document.querySelector('#humid_1');
+                humid1.textContent = 'Humidity: ' + data.daily[1].humidity + '%';
+                let humid2 = document.querySelector('#humid_2');
+                humid2.textContent = 'Humidity: ' + data.daily[2].humidity + '%';
+                let humid3 = document.querySelector('#humid_3');
+                humid3.textContent = 'Humidity: ' + data.daily[3].humidity + '%';
+                let humid4 = document.querySelector('#humid_4');
+                humid4.textContent = 'Humidity: ' + data.daily[4].humidity + '%';
+                let humid5 = document.querySelector('#humid_5');
+                humid5.textContent = 'Humidity: ' + data.daily[5].humidity + '%';
 
             })
         }
@@ -95,7 +134,8 @@ function getForecast(latitude, longitude) {
 
 //define function to render search history
 function printCityList() {
-    //To Do - clear previous selection before appending
+    //clear previous selection before appending
+
     for (i = 0; i < cityList.length; i++) {
 
         let searchHistoryEl = document.createElement('div');
